95d2a19b458a5adbc4793e2c9429cc0a053f3e23
vitisense.lopez <antoniolopez.2022.23@gmail.com>
2025-11-07 19:00:51 +0100

    Cambiar texto del navbar de 'Mi perfil' a 'Cuaderno de campo'

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -93,7 +93,7 @@ export default function Profile() {
   };
 
   return (
-    <div className="min-h-screen bg-gradient-to-b from-green-50 to-green-100 py-6 px-4 sm:px-8">
+    <div className="min-h-screen bg-gradient-to-b from-green-50 to-green-100 py-2 px-4 sm:px-8">
       <div className="max-w-6xl mx-auto bg-white border border-gray-200 shadow-2xl rounded-2xl p-10">
         <h2 className="text-4xl font-bold text-green-800 text-center mb-4">
   🌿 Cuaderno de campo
